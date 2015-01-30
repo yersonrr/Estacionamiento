95ad0e9dd50bfbfed35b12294e529d59498cf6e0
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-29 23:27:50 -0430

    Fixed test suite

--- TestSuite.py
+++ TestSuite.py
@@ -1,17 +1,38 @@
 import unittest
-from taller3 import main
+from taller3ver2 import Estacionamiento
 
 class TestSuite(unittest.TestCase):
     def testASimple(self):
+        e = Estacionamiento(10)
+        e.agregarI(800, 1000)
+        e.Reservar(1000,5000)
         self.assertEqual(main('test1.txt'), True);
 
     def testSimple2(self):
+        e = Estacionamiento(10)
+        e.agregarI(800, 1000)
+        e.agregarI(1000,1200)
+        e.Reservar(900,1100)
+        e.Reservar(1700,1800)
+        e.Reservar(1000,5000)
         self.assertEqual(main('test3.txt'), True);
 
     def testError(self):
+        e = Estacionamiento(10)
+        e.agregarI(800, 1000)
+        e.agregarI(1000,1200)
+        e.Reservar(900,1100)
+        e.Reservar(1700,1800)
+        e.Reservar(1000,5000)
         self.assertEqual(main('test4.txt'), True);
 
     def testFalse(self):
+        e = Estacionamiento(10)
+        e.agregarI(800, 1000)
+        e.agregarI(1000,1200)
+        e.Reservar(900,1100)
+        e.Reservar(1700,1800)
+        e.Reservar(1000,5000)
         self.assertEqual(main('test2.txt'), False);
 
 if __name__ == '__main__':
--- taller3ver2.py
+++ taller3ver2.py
@@ -49,9 +49,3 @@ class Estacionamiento:
 			print('Reservacion guardada satisfactoriamente')
 			return True
 
-e = Estacionamiento(10)
-e.agregarI(800, 1000)
-e.agregarI(1000,1200)
-e.Reservar(900,1100)
-e.Reservar(1700,1800)
-e.Reservar(1000,5000)
